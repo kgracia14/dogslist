84b9b3dd213862771b776d72e304a7071440a5d3
Kyle Gracia <kylepgracia@gmail.com>
2018-10-31 13:36:35 +0100

    commit prior to splitting code into dogliscontainer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import DogsList from './components/DogsList';
 
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>React: DogsList</h1>
-        </header>
+        
         <main>
 
           <DogsList />
